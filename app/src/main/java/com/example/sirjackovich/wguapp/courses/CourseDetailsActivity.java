dda2894d84e160e4f0dda09fb556c7e3bbe6e636
Jacob Reid <jacob.reid@familysearch.org>
2018-09-29 17:05:38 -0600

    Course Mentor list showing up

--- app/src/main/java/com/example/sirjackovich/wguapp/courses/CourseDetailsActivity.java
+++ app/src/main/java/com/example/sirjackovich/wguapp/courses/CourseDetailsActivity.java
@@ -1,18 +1,27 @@
 package com.example.sirjackovich.wguapp.courses;
 
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CursorAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import com.example.sirjackovich.wguapp.DatabaseHelper;
 import com.example.sirjackovich.wguapp.R;
+import com.example.sirjackovich.wguapp.mentors.MentorsProvider;
 
-public class CourseDetailsActivity extends AppCompatActivity {
+public class CourseDetailsActivity extends ActionBarActivity implements LoaderManager.LoaderCallbacks<Cursor>{
   private EditText title;
   private EditText startDate;
   private EditText endDate;
@@ -20,11 +29,33 @@ public class CourseDetailsActivity extends AppCompatActivity {
   private EditText note;
   private String action;
   private String courseFilter;
+  private CursorAdapter cursorAdapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_course_details);
+
+    String[] from = {DatabaseHelper.MENTOR_NAME};
+    int[] to = {android.R.id.text1};
+
+    cursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, null, from, to, 0);
+
+    ListView listView = (ListView) findViewById(R.id.listView);
+
+    listView.setAdapter(cursorAdapter);
+
+//    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//      @Override
+//      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//        Intent intent = new Intent(CoursesActivity.this, CourseDetailsActivity.class);
+//        Uri uri = Uri.parse(CoursesProvider.CONTENT_URI + "/" + id);
+//        intent.putExtra(CoursesProvider.CONTENT_ITEM_TYPE, uri);
+//        startActivityForResult(intent, 1);
+//      }
+//    });
+
+    getLoaderManager().initLoader(0, null, this);
 
 
     title = (EditText) findViewById(R.id.title_edit_text);
@@ -94,5 +125,21 @@ public class CourseDetailsActivity extends AppCompatActivity {
     values.put(DatabaseHelper.COURSE_NOTE, note.getText().toString().trim());
     getContentResolver().insert(CoursesProvider.CONTENT_URI, values);
     setResult(RESULT_OK);
+  }
+
+  @Override
+  public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+    return new CursorLoader(this, MentorsProvider.CONTENT_URI,
+      null, null, null, null);
+  }
+
+  @Override
+  public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+    cursorAdapter.swapCursor(data);
+  }
+
+  @Override
+  public void onLoaderReset(Loader<Cursor> loader) {
+    cursorAdapter.swapCursor(null);
   }
 }
